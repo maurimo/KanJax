7adb6c7f7078dba6a5d315023fa81345600eeebd
Maurizio Monge <maurizio.monge@im.ufrj.br>
2015-12-20 13:51:23 -0200

    templetized

--- data.php
+++ data.php
@@ -1,17 +1,37 @@
 <?php
-  error_reporting(-1);
-  ini_set('display_errors', 'On');
-  try {
-    $db_handle  = new SQLite3('kanji_it.db', SQLITE3_OPEN_READONLY);
-  } catch (Exception $e) {
-    exit(json_encode(Array("status" => "ERR_CANT_READ_DB", "message" => $e->getMessage())));
-  }
-  $statement = $db_handle->prepare('SELECT * FROM KanjiIinfo WHERE kanji = :kanji;');
-  $statement->bindValue(':kanji', $_GET["kanji"]);
-  $result = $statement->execute();
-  $row = $result->fetchArray(SQLITE3_ASSOC);
-  if(!$row)
-    exit(json_encode(Array("status" => "ERR_NOT_FOUND")));
-  $response = Array("status" => "OK", "data" => $row);
-  echo json_encode($response);
+
+// Settings
+$DATABASE = "kanji_it.db";
+$DEBUG = true;
+
+if($DEBUG) {
+    error_reporting(-1);
+    ini_set('display_errors', 'On');
+}
+
+if(empty($_GET["kanji"]))
+    exit(json_encode(Array(
+        "status" => "ERR_WRONG_QUERY",
+        "message" => "Variable \"kanji\" not set"
+    )));
+
+try {
+    $db_handle  = new SQLite3($DATABASE, SQLITE3_OPEN_READONLY);
+} catch (Exception $e) {
+    exit(json_encode(Array(
+        "status" => "ERR_CANT_READ_DB",
+        "message" => $e->getMessage()
+    )));
+}
+$statement = $db_handle->prepare('SELECT * FROM KanjiIinfo WHERE kanji = :kanji;');
+$statement->bindValue(':kanji', $_GET["kanji"]);
+$result = $statement->execute();
+$row = $result->fetchArray(SQLITE3_ASSOC);
+if(!$row)
+    exit(json_encode(Array(
+        "status" => "ERR_NOT_FOUND",
+        "message" => "Key \"" . $_GET["kanji"] . "\" not found in DB."
+    )));
+$response = Array("status" => "OK", "data" => $row);
+echo json_encode($response);
 ?>
--- kanjax.js
+++ kanjax.js
@@ -1,109 +1,26 @@
 
-kanji_info = {
-	"昼": [
-    "ore diurne",
-    "ore diurne, a mezzogiorno",
-    "Roosters in Japan are very civilised, they play the shakuhachi at nightbreak to signify the beginning of daytime.",
-    "E698BC.png",
-    "チュウ",
-    "ひる"
-	],
-	"御": [
-    "onorevole",
-    "onorevole, manipolare, governare",
-    "Una persona ONOREVOLE per nessuna ragione salta la FILA alle casse della VENDITA ALL'INGROSSO per ANDARE avanti!",
-    "E5BEA1.png",
-    "ギョ、ゴ",
-    "おん-、お-、み-"
-  ],
-	"飯": [
-    "pasto",
-    "pasto, riso bollito",
-    "I´m anti-fastfood. Sit down and eat a real meal!",
-    "E9A3AF.png",
-    "ハン",
-    "めし"
-  ]
-};
-
 KanJax = {
-		DEFAULT_POPUP_CSS:
-		    '.kanjax {\n' +
-				'  color: #a00000;\n' +
-				'  cursor: hand;\n' +
-				'}\n' +
-				'#kanjax_popup {\n' +
-				'  width: 700px;\n' +
-				'  display:none;\n' +
-				'  background-color: #e0e0ff;\n' +
-				'  border: 1px solid #8080ff;\n' +
-				'  padding: 1.2em;\n' +
-				'  border-radius: 0.8em;\n' +
-				'  text-align: center;\n' +
-				'}\n' +
-				'#kanjax_p2opup #strokes {\n' +
-				'  width: 700px;\n' +
-				'}\n' +
-				'#kanjax_popup #on {\n' +
-				'  font-style: normal;\n' +
-				'  font-size: 24px;\n' +
-				'}\n' +
-				'#kanjax_popup #kun {\n' +
-				'  font-style: normal;\n' +
-				'  font-size: 24px;\n' +
-				'}\n' +
-				'#kanjax_popup #kanji {\n' +
-				'  padding-right: 0.6em;\n' +
-				'}\n' +
-				'#kanjax_popup #keyword {\n' +
-				'  padding-left: 0.6em;\n' +
-				'}\n' +
-				'#kanjax_popup .japanese {font-family: "Kochi Mincho";}\n' +
-				'#kanjax_popup .small {font-size: 20px;}\n' +
-				'#kanjax_popup .medium {font-size: 24px;}\n' +
-				'#kanjax_popup .large {font-size: 32px;}\n' +
-				'#kanjax_popup .huge {font-size: 50px;}\n' +
-				'#kanjax_popup .bold {font-weight: bold;}\n' +
-				'#kanjax_popup .italic {font-style: italic;}\n',
+		popupContent: "Couldn't load popup template.",
 
-		DEFAULT_POPUP_INNERHTML:
-				'<table border="0" align="center"><tr><td style="text-align: center">\n' +
-				'  <span class="huge japanese" id="kanji"></span>\n' +
-				'</td><td style="text-align: center">\n' +
-				'  <span class="large bold" id="keyword"></span>\n' +
-				'</td></tr></table>\n' +
-				'<div class="medium italic" id="meaning"></div></br>\n' +
-				'<div class="small italic">\n' +
-				'  On: <span id="on" class="japanese"></span>,\n' +
-				'  Kun: <span id="kun" class="japanese"></span></div></br>\n' +
-				'<div class="medium" id="story"></div></br>\n' +
-				'<img id="strokes"/></br>\n' +
-				'<div class="medium" style="margin-top:0.5em">\n' +
-				'  <a href="" id="jisho">Jisho</a>\n' +
-				'  <a href="" id="koohii">Koohii</a>\n' +
-				'</div>',
+		popupCache: {},
 
 		// inserts into the DOM what is necessary to show the default popup.
 		setupDefaultPopup: function() {
-				var div, head, style;
-				
-				head = document.head || document.getElementsByTagName('head')[0],
-				style = document.createElement('style');
-				style.id = 'kanjax_style';
+				var div, style;
 
-				style.type = 'text/css';
-				if (style.styleSheet)
-						style.styleSheet.cssText = KanJax.DEFAULT_POPUP_CSS;
-				else
-						style.appendChild(document.createTextNode(KanJax.DEFAULT_POPUP_CSS));
+				var style = document.createElement("link")
+        style.setAttribute("rel", "stylesheet")
+        style.setAttribute("type", "text/css")
+        style.setAttribute("href", "kanjax/kanjax_popup.css")
+				document.head.appendChild(style);
 
-				head.appendChild(style);
-				
 				div = document.createElement("div");
 				div.id = "kanjax_popup";
 			  div.className = 'kanjax_forbidden';
-				div.innerHTML = KanJax.DEFAULT_POPUP_INNERHTML;
 				document.body.appendChild(div);
+				$.get("kanjax/kanjax_popup_template.html", function(response) {
+						KanJax.popupContent = response;
+				});
 		},
 
 		// cleans all the default popup stuff inserted in the DOM.
@@ -116,41 +33,47 @@ KanJax = {
 		},
 
 		showDefaultPopup: function(info) {
-				$('#kanjax_popup #kanji').prop('innerText', info.kanji);
-				$('#kanjax_popup #keyword').prop('innerText', info.keyword);
-				$('#kanjax_popup #meaning').prop('innerText', info.meaning);
-				$('#kanjax_popup #story').prop('innerText', info.desc);
-				img = new Image();
-				img.onload = function() {
-						$('#kanjax_popup #strokes').prop('src', this.src);
-						w = Math.min(700, 100*this.width/this.height);
-						$('#kanjax_popup #strokes').css({width: w+'px'});     
-				}
-				img.src = 'kanjax/images/'+info.strokes;
-				$('#kanjax_popup #on').prop('innerText', info.onyomi);
-				$('#kanjax_popup #kun').prop('innerText', info.kunyomi);
-				$('#kanjax_popup #jisho').prop('href', 'http://jisho.org/search/%23kanji%20'+kanji);
-				$('#kanjax_popup #koohii').prop('href', 'http://kanji.koohii.com/study/kanji/'+kanji);
-				$('#kanjax_popup').bPopup({
-						speed: 120, 
-						position: ['auto', document.body.clientHeight/2-200]
-				});
+				var div, k, content;
+				div = document.getElementById("kanjax_popup");
+				content = KanJax.popupContent.replace(
+								/\{\{(\w+)\}\}/g,
+						function(match, key) {
+								return info[key] || ("{unknown field "+key+"}");
+						});
+				div.innerHTML = content;
+				$(div).bPopup({ speed: 120 });
+		},
+
+		showErrorPopup: function(info) {
+				var div, k, content;
+				div = document.getElementById("kanjax_popup");
+				content = "<h2>Error!</h2>" + info.status;
+				if(info.message)
+						content += "<br/>" + info.message;
+				div.innerHTML = content;
+				$(div).bPopup({ speed: 120 });
 		},
 
 		// default click handler, uses jQuery + bPopup to show a nice popup
-		// FIX-ME: transform hardcoded sizes into settings
 		activateDefaultPopup: function(e) {
 				var kanji, info, img, w;
 				e.preventDefault();
 				kanji = e.currentTarget.innerText;
-				
+				if(kanji in KanJax.popupCache) {
+						KanJax.showDefaultPopup(KanJax.popupCache[kanji]);
+						return;
+				}
+
 				$.ajax({url: "kanjax/data.php?kanji="+kanji, success: function(result){
 						result = $.parseJSON(result);
-						if(result.status == "OK")
+						if(result.status == "OK") {
+								KanJax.popupCache[kanji] = result.data;
 								KanJax.showDefaultPopup(result.data);
+						}
+						else {
+								KanJax.showErrorPopup(result);	
+						}
 				}});
-				//info = kanji_info[kanji] || ['?', '?', '?', '', '?', '?'];				
-				//KanJax.showDefaultPopup(kanji, info);
     },
 
 		// Matches a string starting with a kanji
@@ -239,6 +162,24 @@ KanJax = {
 						else
 								n.remove();
 				}
+		},
+
+		setup : function() {
+				var style;
+				if(!document.getElementById("kanjax_css")) {
+						style = document.createElement("link");
+						style.id = "kanjax_css";
+						style.setAttribute("rel", "stylesheet");
+						style.setAttribute("type", "text/css");
+						style.setAttribute("href", "kanjax/kanjax.css");
+						document.head.appendChild(style);
+				}
+		},
+
+		cleanup: function() {
+				var el;
+				if(el = document.getElementById('kanjax_css'))
+						el.remove();
 		}
 };
 
@@ -247,6 +188,7 @@ KanJax = {
 		// DOM Ready
 		$(function() {
 				KanJax.setupDefaultPopup();
+				KanJax.setup();
 				KanJax.addLinks();
 		});
 
