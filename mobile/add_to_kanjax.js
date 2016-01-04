42a9b8f24a3d1e28beb5f39b630f3d5ba8119787
Maurizio Monge <maurizio.monge@im.ufrj.br>
2016-01-04 01:49:13 -0200

    variants of style and script to make kanjax work on mobile devices

---
+++ mobile/add_to_kanjax.js
@@ -0,0 +1,7 @@
+
+$(function(){
+    KanJax.loadStaticJSON = true;
+    //KanJax.forcePopupPositionX = 10;
+    //KanJax.forcePopupPositionY = 20;
+    KanJax.basicInstall();
+});
---
+++ mobile/update_phone.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cat ../kanjax/kanjax.js add_to_kanjax.js >kanjax.js
+cp kanjax_popup_phone.css kanjax_popup.css
+
+scp -P 8022 kanjax_popup_template.static.html kanjax_popup.css kanjax.css kanjax.js 192.168.0.132:/storage/sdcard0/AnkiDroid/collection.media/kanjax
+
+rm kanjax.js kanjax_popup.css
